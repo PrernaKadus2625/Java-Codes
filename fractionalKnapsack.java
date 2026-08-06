e901aef7a5f22fd39dd89cd7cb757959ee20d8bb
PrernaKadus2625 <prernakadus7342@gmail.com>
2026-08-06 21:51:45 +0530

    Add files via upload

---
+++ fractionalKnapsack.java
@@ -0,0 +1,40 @@
+package GreedyAlgorithm;
+import java.util.*;
+
+public class fractionalKnapsack {
+    
+    public static void main(String args[]) {
+        int val[] = {60, 100, 120};
+        int weight[] = {10, 20, 30};
+        int W = 50;
+
+        double ratio[][] = new double[val.length][2];
+        // 0th col => idx; 1st col => ratio
+
+        for (int i = 0; i < val.length; i++) {
+            ratio[i][0] = i;
+            ratio[i][1] = val[i] / (double) weight[i];
+        }
+
+        // sort by ratio in ascending order
+       Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
+
+        int capacity = W;
+        double finalVal = 0; // Use double because fractional value may be added
+
+        for (int i = ratio.length - 1; i >= 0; i--) {
+            int idx = (int) ratio[i][0];
+            if (capacity >= weight[idx]) { // include full item
+                finalVal += val[idx];
+                capacity -= weight[idx];
+            } else { // include fractional item
+                finalVal += ratio[i][1] * capacity; // ratio * remaining capacity
+                capacity = 0;
+                break; // knapsack is full
+            }
+        }
+
+        System.out.println("Maximum value we can obtain = " + finalVal);
+    }
+}
+
