1840af2b03ba0d0f83a1500ad0142ad2a74569db
PrernaKadus2625 <prernakadus7342@gmail.com>
2026-08-06 22:27:22 +0530

    Add files via upload

---
+++ removeduplication.java
@@ -0,0 +1,33 @@
+package recursion;
+
+public class removeduplication {
+    public static void remove(String str , int idx, StringBuilder newstr , boolean map[]){
+        if(idx ==str.length()){
+            System.out.println(newstr);
+            return;
+        }
+        char currchar = str.charAt(idx);
+        if ( map[currchar - 'a'] == true){
+            remove(str,idx + 1, newstr , map);
+        }
+        else {
+             map[currchar - 'a'] = true;
+              remove(str,idx + 1, newstr.append(currchar) , map);
+        }
+    }
+        public static void main( String args[]){
+            String str = "appnacelgge";
+            remove(str , 0 , new StringBuilder(""), new boolean[26]);
+
+        }
+ 
+}
+
+
+        
+
+        
+
+    
+    
+
