1627e9c0ce4024f27620e132c401edee4f5d2e15
PrernaKadus2625 <prernakadus7342@gmail.com>
2026-08-06 21:30:07 +0530

    Add files via upload

---
+++ maximum.java
@@ -0,0 +1,27 @@
+package ArrayList;
+import java.util.ArrayList;
+
+public class maximum {
+    public static void main(String args[]){
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(5);
+    list.add(8);
+    list.add(9);
+    list.add(6);
+    int max = Integer.MIN_VALUE;
+    for(int i = 0;i<list.size();i++){
+        if(list.get(i)> max){
+            max = list.get(i);
+        }
+        //OR max = Math.max(max , list.get(i));
+    }    
+    System.out.println(max);
+
+    
+
+
+
+    
+}
+
+}
