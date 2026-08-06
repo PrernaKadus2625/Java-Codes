85cc3ef9619c06e23c60f78683c3b9963eae4421
PrernaKadus2625 <prernakadus7342@gmail.com>
2026-08-06 21:20:38 +0530

    Add files via upload

---
+++ LCAInBinaryTree.java
@@ -0,0 +1,74 @@
+package Binary_Tree;
+
+import java.util.ArrayList;
+
+public class LCAInBinaryTree {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+        if (root == null) {
+            return false;
+        }
+
+        path.add(root);
+
+        if (root.data == n) {
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if (foundLeft || foundRight) {
+            return true;
+        }
+
+        // Backtracking step
+        path.remove(path.size() - 1);
+        return false;
+    }
+
+    public static Node lca(Node root, int n1, int n2) {
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        // Get paths from root to n1 and n2
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        // Find last common node in both paths
+        int i = 0;
+        for (; i < path1.size() && i < path2.size(); i++) {
+            if (path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+
+        return path1.get(i - 1);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        Node lcaNode = lca(root, 4, 5);
+        System.out.println("LCA of 4 and 5 is: " + lcaNode.data);
+    }
+}
+
