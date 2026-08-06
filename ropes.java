55204bb3bc16af99eb99442110ea5377ac20a1f0
PrernaKadus2625 <prernakadus7342@gmail.com>
2026-08-06 21:56:39 +0530

    Add files via upload

---
+++ ropes.java
@@ -0,0 +1,28 @@
+package Heaps;
+import java.util.*;
+
+public class ropes {
+
+    public static void main(String args[]){
+        int ropes[] = {2,3,3,4,6};
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0 ; i < ropes.length ;i++){
+              pq.add(ropes[i]);
+
+        }
+
+        int cost = 0;
+        while(pq.size() > 1){
+        int min = pq.remove();
+        int min1 = pq.remove();
+
+        cost += min + min1;
+        pq.add(min + min1);
+        
+        }
+        System.out.print("cost = " + cost);
+
+    }
+    
+}
