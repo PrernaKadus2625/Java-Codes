41ace2fef05980458e8c4f629ec980f5d25d8a60
PrernaKadus2625 <prernakadus7342@gmail.com>
2026-08-06 22:27:02 +0530

    Add files via upload

---
+++ mergesortt.java
@@ -0,0 +1,68 @@
+package recursion;
+
+public class mergesortt {
+
+    public static void printArray(int arr[]) {
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i] + " ");
+    }
+    System.out.println();  // for new line
+}
+
+    
+
+    public static void sort(int arr[],int si ,int ei){
+        if(si>=ei){
+            return;
+        }
+        int mid = (si + ei)/2 ;
+        sort(arr , si , mid);
+        sort(arr,mid+1 ,ei );
+        merge(arr , si , ei ,mid);
+    }
+    public static void merge(int arr[],int si,int ei,int mid){
+        int temp[] = new int [ei-si+1];
+        int i = si;    //idx for first sorted part 
+        int j = mid +1 ; // for second sorted part 
+        int k = 0;       // idx for temp
+        
+        while(i<= mid && j<=ei){
+            if (arr[i]> arr [j]){
+                temp[k]= arr[i];
+                i++;
+            }
+            else{
+                temp[k]= arr[j];
+                j++;
+            }
+            k++;
+        }
+        // for leftover elements in 1st sorted part
+        while(i<=mid){
+            temp[k++] = arr[i++];
+        }
+
+         // for leftover elements in 2nd sorted part
+        while(j<=ei){
+            temp[k++] = arr[j++];
+        }
+
+        //copy temp to original array
+        for( k=0, i=si ;k< temp.length;k++,i++){
+            arr[i]= temp[k];
+        }
+    }
+
+        public static void main(String args[]){
+            int arr[]= {6,8,2,9,1,5};
+            sort(arr,0,arr.length-1);
+            printArray(arr);
+
+        }
+
+
+
+    
+    
+}
+
